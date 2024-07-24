963f8007a1c8bf8acf47ade52536769c8ef12e4b
emalek383 <166419206+emalek383@users.noreply.github.com>
2024-07-24 15:08:03 +0200

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -26,7 +26,7 @@ def get_data(stocks, start, end):
         stockData: pd.DataFrame with close of stock data
     """
     # Disable progress bar
-    yf.pdr_override()
+    #yf.pdr_override()
     yf.set_tz_cache_location(None)
         
     with suppress_stdout():
